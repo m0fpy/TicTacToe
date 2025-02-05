090541c253fd7fe6689e6e2d102a67866ca4cedf
m0fpy <eroshkevich.matvey.mofp@gmail.com>
2025-02-04 23:12:01 -0800

    Update MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -57,6 +57,13 @@ namespace TicTacToe
             EndScreen.Visibility = Visibility.Visible;
         }
 
+        private void TransitionToGameScreen()
+        {
+            EndScreen.Visibility = Visibility.Hidden;
+            TurnPanel.Visibility = Visibility.Visible;
+            GameCanvas.Visibility = Visibility.Visible;
+        }
+
         private void OnMoveMade(int r, int c)
         {
             Player player = gameState.GameGrid[r, c];
@@ -80,7 +87,16 @@ namespace TicTacToe
 
         private void OnGameRestarted()
         {
+            for (int r = 0; r < 3; r++)
+            {
+                for(int c = 0; c < 3; c++)
+                {
+                    imageControls[r, c].Source = null;
+                }
+            }
 
+            PlayerImage.Source = imageSources[gameState.CurrentPlayer];
+            TransitionToGameScreen();
         }
 
         private void GameGrid_MouseDown(object sender, MouseButtonEventArgs e)
@@ -95,7 +111,7 @@ namespace TicTacToe
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-
+            gameState.Reset();
         }
     }
 }
