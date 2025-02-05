4381ee3a3bf486535097cfd1a2f7780601ed195b
m0fpy <eroshkevich.matvey.mofp@gmail.com>
2025-02-04 22:13:13 -0800

    GameResult and GameState classes

---
+++ Scripts/GameResult.cs
@@ -0,0 +1,10 @@
+﻿using TicTacToe.Scripts.Enums;
+
+namespace TicTacToe.Scripts
+{
+    public class GameResult
+    {
+        public Player Winner { get; set; }
+        public WinInfo WinInfo { get; set; }
+    }
+}
