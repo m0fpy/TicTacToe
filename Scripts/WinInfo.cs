18e0b14d6487b2702e517dcd84ee291616756910
m0fpy <eroshkevich.matvey.mofp@gmail.com>
2025-02-04 22:11:55 -0800

    Create WinInfo.cs

---
+++ Scripts/WinInfo.cs
@@ -0,0 +1,10 @@
+﻿using TicTacToe.Scripts.Enums;
+
+namespace TicTacToe.Scripts
+{
+    public class WinInfo
+    {
+        public WinType Type { get; set; }
+        public int Number {  get; set; }
+    }
+}
