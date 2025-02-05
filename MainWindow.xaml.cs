0b9360836bab39d9d6f41820352f00d7a582f1f7
m0fpy <eroshkevich.matvey.mofp@gmail.com>
2025-02-04 23:06:03 -0800

    UI stuff

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,17 +8,94 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TicTacToe.Scripts;
+using TicTacToe.Scripts.Enums;
 
 namespace TicTacToe
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
+        private readonly Dictionary<Player, ImageSource> imageSources = new()
+        {
+            {Player.X, new BitmapImage(new Uri("pack://application:,,,/Assets/X15.png")) },
+            {Player.O, new BitmapImage(new Uri("pack://application:,,,/Assets/O15.png")) }
+        };
+
+        private readonly Image[,] imageControls = new Image[3, 3];
+        private readonly GameState gameState = new GameState();
+
         public MainWindow()
         {
             InitializeComponent();
+            SetupGameGrid();
+
+            gameState.MoveMade += OnMoveMade;
+            gameState.GameEnded += OnGameEnded;
+            gameState.GameRestarted += OnGameRestarted;
+        }
+
+        private void SetupGameGrid()
+        {
+            for (int r = 0; r < 3; r++)
+			{
+                for(int c = 0; c < 3; c++)
+                {
+                    Image imageControl = new Image();
+                    GameGrid.Children.Add(imageControl);
+                    imageControls[r, c] = imageControl;
+                }
+			}
+        }
+
+        private void TransitionToEndScreen(string text, ImageSource winnerImage)
+        {
+            TurnPanel.Visibility = Visibility.Hidden;
+            GameCanvas.Visibility = Visibility.Hidden;
+            ResultText.Text = text;
+            WinnerImage.Source = winnerImage;
+            EndScreen.Visibility = Visibility.Visible;
+        }
+
+        private void OnMoveMade(int r, int c)
+        {
+            Player player = gameState.GameGrid[r, c];
+            imageControls[r, c].Source = imageSources[player];
+            PlayerImage.Source = imageSources[gameState.CurrentPlayer];
+        }
+
+        private async void OnGameEnded(GameResult gameResult)
+        {
+            await Task.Delay(1000);
+
+            if (gameResult.Winner == Player.None)
+            {
+                TransitionToEndScreen("It's a tie!", null);
+            }
+            else
+            {
+                TransitionToEndScreen("Winner: ", imageSources[gameResult.Winner]);
+            }
+        }
+
+        private void OnGameRestarted()
+        {
+
+        }
+
+        private void GameGrid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            double squareSize = GameGrid.Width / 3;
+            Point clickPosition = e.GetPosition(GameGrid);
+            int row = (int)(clickPosition.Y / squareSize);
+            int col = (int)(clickPosition.X / squareSize);
+            gameState.MakeMove(row, col);
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
