cf147873888a7c7878c51c58cd27fd6be8008ead
m0fpy <eroshkevich.matvey.mofp@gmail.com>
2025-02-05 20:49:41 -0800

    Update MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -52,6 +52,7 @@ namespace TicTacToe
         {
             TurnPanel.Visibility = Visibility.Hidden;
             GameCanvas.Visibility = Visibility.Hidden;
+            Line.Visibility = Visibility.Hidden;
             ResultText.Text = text;
             WinnerImage.Source = winnerImage;
             EndScreen.Visibility = Visibility.Visible;
@@ -64,7 +65,40 @@ namespace TicTacToe
             GameCanvas.Visibility = Visibility.Visible;
         }
 
-        
+        private (Point, Point) FindLinePoints(WinInfo winInfo)
+        {
+            double squareSize = GameGrid.Width / 3;
+            double margin = squareSize / 2;
+
+            if(winInfo.Type == WinType.Row)
+            {
+                double y = winInfo.Number * squareSize + margin;
+                return (new Point(0, y), new Point(GameGrid.Width, y));
+            }
+            if(winInfo.Type == WinType.Column)
+            {
+                double x = winInfo.Number * squareSize + margin;
+                return ( new Point(x, 0), new Point(x, GameGrid.Height));
+            }
+            if(winInfo.Type == WinType.MainDiagonal)
+            {
+                return (new Point(0, 0), new Point(GameGrid.Width, GameGrid.Height));
+            }
+            
+            return (new Point(GameGrid.Width, 0), new Point(0, GameGrid.Height));
+        }
+
+        private void ShowLine(WinInfo winInfo)
+        {
+            (Point start, Point end) = FindLinePoints(winInfo);
+            Line.X1 = start.X;
+            Line.Y1 = start.Y;
+
+            Line.X2 = end.X;
+            Line.Y2 = end.Y;
+
+            Line.Visibility = Visibility.Visible;
+        }
 
         private void OnMoveMade(int r, int c)
         {
@@ -83,6 +117,8 @@ namespace TicTacToe
             }
             else
             {
+                ShowLine(gameResult.WinInfo);
+                await Task.Delay(1000);
                 TransitionToEndScreen("Winner: ", imageSources[gameResult.Winner]);
             }
         }
